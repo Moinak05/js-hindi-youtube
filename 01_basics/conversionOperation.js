4e712518b580f29e1c320adc04a56312846f12c7
Moinak05 <papaimaiti05@gmail.com>
2024-11-14 03:05:20 +0000

    Conversion of Datatypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "20abc";
+console.log(typeof score);
+let valueInnumber = Number(score)
+console.log(typeof valueInnumber)
+console.log(valueInnumber)
+//conversion:
+//33=>33
+//33abc=>NaN
+//TRUE=>1;FALSE=>0
+let isLoggedIn = 0
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+//1=>true
+//0=>false
+//""=>false
+//"moinak=>true
+let number = 56
+let stringnumber = String(number)
+console.log(stringnumber)
+console.log(typeof stringnumber)
