e4d2e0975ae28c9ee4348c1e58aae098120054c9
Moinak05 <papaimaiti05@gmail.com>
2024-11-14 03:29:33 +0000

    conversion

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,20 +1,37 @@
 let score = "20abc";
-console.log(typeof score);
+// console.log(typeof score);
 let valueInnumber = Number(score)
-console.log(typeof valueInnumber)
-console.log(valueInnumber)
+// console.log(typeof valueInnumber)
+// console.log(valueInnumber)
 //conversion:
 //33=>33
 //33abc=>NaN
 //TRUE=>1;FALSE=>0
 let isLoggedIn = 0
 let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn)
+// console.log(booleanisLoggedIn)
 //1=>true
 //0=>false
 //""=>false
 //"moinak=>true
 let number = 56
 let stringnumber = String(number)
-console.log(stringnumber)
-console.log(typeof stringnumber)
+// console.log(stringnumber)
+// console.log(typeof stringnumber)
+//                          ***************OPERATIONS*************  
+let num = 5
+let negnum = -(num)
+// console.log(negnum)
+let str1 ="Hellow"
+let str2 = " Moinak"
+str3= str1 + str2
+// console.log(str3);
+// console.log("1"+2);
+// console.log(1+"2")
+// console.log("1"+2+3)
+// console.log(1+2+"3")
+// console.log(+true);
+// console.log(+"");
+let gameCounter = 100
+++gameCounter ;
+console.log(gameCounter)
