23bfd5a0237078139ac1da4d5c70d3028a1f1e0e
Moinak05 <papaimaiti05@gmail.com>
2024-11-14 02:42:18 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 1
+let accountEmail = "moinak14@gmail.com"
+var accountPassword ="54664"
+accountCity = "Kolkata"
+//accountId =2// #constant can't be change
+
+accountEmail ="papai@gmail.com"
+accountPassword ="3565"
+accounyCity ="Jaipur"
+let accountState;
+
+console.log(accountId)
+console.table([accountId ,accountEmail, accountPassword, accountCity,accountState])
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict";//treat all JS code as newer version.
+//alert(3+3),because we are using node Js not browser
+console.log(3+3)
+console.log("Moinak")
+let name ="Hitesh"//string datatype
+let age=19
+let isLoggedIn=false//boolean datatype
+//let state;[in this case this is undefied]
+//let state=null[in this case this is a null datatype]
+console.log(typeof "Moinak")
+console.log(typeof true)
+console.log(typeof null)
