832e82485e466f68b28cf1a28923f43a3c2f5939
Moinak05 <papaimaiti05@gmail.com>
2024-11-18 14:09:18 +0000

    nums_and_maty

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -2,8 +2,14 @@
 // const studyhour = 12
 //console.log(name + studyhour + " Hours")
 //console.log(`My name is ${name} and my study hour is ${studyhour} Hours`) // nEW VERSION TO WRITE
-const gameName = new String ('moinakgh')
+const gameName = new String ('moinak-gh-com')
 
 // console.log(gameName [0]);
 // console.log(gameName.length);
-console.log(gameName.charAt(2))
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('t'))
+const url = "https://moinak.com/moinak%20maiti"
+console.log(url.replace("%20","-"))
+console.log(url.includes("moinak")) //.includes is used for whether the string is availabe in that url or not
+console.log(url.includes('maity'))
+console.log(gameName.split('-'))
---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,26 @@
+const score = 500 
+// console.log(score);
+const balance = new Number (500)  // new is a key word which transform to a number
+// console.log(balance);
+// console.log(balance.toString().length) ;
+// console.log(balance.toFixed(2)) ;
+const anotherNumber = 75.5264 ;
+// console.log (anotherNumber.toPrecision(3))
+const hundred = 200000000
+// console.log(hundred.toLocaleString('en-IN')) // by using localeString() we get the commas in usa manner ; 
+
+
+//++++++++++++++++++++++  Maths  +++++++++++++++++++++++++++++++
+// console.log(Math)
+// console.log(Math.abs(-9))//abs means absolute value ..positive remains positive but negative turns to positive
+// console.log(Math.abs(5))
+// console.log(Math.round(52.256))
+// console.log(Math.round(52.569))
+// console.log(Math.ceil(4.5))
+// console.log(Math.floor(4.5))
+// console.log(Math.max(8,56,85,6,98,5,6,5,8502,2.3))
+// console.log(Math.min(8,56,85,6,98,5,6,5,8502,2.3))
+console.log(Math.random())
+console.log(Math.floor(Math.random()*10))
+const min =10 
+const max = 20
